0c04c4b99ee1a0a60a5c140380234ab856e5174b
Phil Underwood <beardydoc@gmail.com>
2020-08-05 16:51:29 +0100

    more fettling

--- make_exe.bat
+++ make_exe.bat
@@ -1,4 +1,2 @@
-#!/bin/sh
-VERSION=`git describe --tags`
-pyupdater build --app-version 1.0.0  --add-binary="libusb-1.0.dll;." --add-data="doc/manual.pdf;." --onefile PonyTrainer.py
-pyupdater pkg -p -s
+pyupdater make-spec  --add-binary="libusb-1.0.dll;." --add-data="doc/manual.pdf;." --onefile PonyTrainer.py
+cat *.spec
