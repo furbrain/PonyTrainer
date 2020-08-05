6e30b5cf1ce921c6e49a72337bb3b6a651575bea
Phil Underwood <beardydoc@gmail.com>
2020-08-05 22:17:05 +0100

    Refactor bits

--- make_exe.bat
+++
@@ -1,2 +0,0 @@
-pyupdater build -D -pyinstaller-log-info --app-version %APPVEYOR_BUILD_VERSION% windows.spec
-pyupdater pkg -p -s
--- make_linux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-pyupdater build -D -pyinstaller-log-info --app-version $APPVEYOR_BUILD_VERSION linux.spec
-pyupdater pkg -p -s
