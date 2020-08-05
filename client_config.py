7473d39e478dddb2796550a42b7c234f482296f8
Phil Underwood <beardydoc@gmail.com>
2020-08-05 14:53:55 +0100

    Various fixes

---
+++ client_config.py
@@ -0,0 +1,7 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'None'
+    APP_NAME = 'PonyTrainer'
+    COMPANY_NAME = 'Underwood Underground'
+    HTTP_TIMEOUT = 30
+    MAX_DOWNLOAD_RETRIES = 3
+    UPDATE_URLS = ['http://shetlandattackpony.co.uk/releases/']
