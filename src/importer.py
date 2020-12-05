64bcde691d1e13c17880ff6958b8a48daac962e8
Phil Underwood <beardydoc@gmail.com>
2020-12-05 01:07:55 +0000

    Update importer.py

--- src/importer.py
+++ src/importer.py
@@ -31,11 +31,11 @@ class ActualImportDialog(gui.ImportDialog):
         print("surveys: ")
         pprint(self.surveys)
         for idx, s in enumerate(self.surveys.values()):
-            self.survey_list.InsertItem(idx, s['time'].strftime("%Y-%m-%d %Hh%M"))
-            self.survey_list.SetItem(idx, 1, str(s['station_count']))
-            self.survey_list.SetItem(idx, 2, str(s['leg_count']))
-            print("setting item: ", idx, "to: ", s['survey'])
-            self.survey_list.SetItemData(idx, s['survey'])
+            i = self.survey_list.InsertItem(idx, s['time'].strftime("%Y-%m-%d %Hh%M"))
+            self.survey_list.SetItem(i, 1, str(s['station_count']))
+            self.survey_list.SetItem(i, 2, str(s['leg_count']))
+            print("setting item: ", id "to: ", s['survey'])
+            self.survey_list.SetItemData(i, s['survey'])
         for i in range(len(self.surveys)):
             print("getting item: ", i, "is: ", self.survey_list.GetItemData(i))
         self.survey_list.SortItems(self.sort_list)
